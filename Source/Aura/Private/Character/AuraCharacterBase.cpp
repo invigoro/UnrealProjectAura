4dfe1211caeb8214ecd8ddafb4c9a9efd28a0e19
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2024-03-14 16:42:12 -0700

    other base classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Yo Mama
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Yo Mama
+
+
+#include "Character/AuraEnemy.h"
+
