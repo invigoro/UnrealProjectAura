50536fb158574c5dc9e8719a2a867e22a2ab8e0e
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2024-03-14 23:26:03 -0700

    highlight

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,18 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include <Interaction/EnemyInterface.h>
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -55,4 +63,43 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit)
+	{
+		return;
+	}
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/**
+	* Line trace from cursor. There are several scenarios:
+	* A. LastActor == ThisActor
+	*	- do nothing.
+	* B. LastActor is null && ThisActor is valid
+	*	- Highlight ThisActor
+	* C. LastActor is valid && ThisActor is null 
+	*	- Unhighlight LastActor
+	* D. Both actors are valid, but LastActor != ThisActor
+	*  - Unhighlight LastActor, highlight ThisActor
+	*/
+	if (LastActor == ThisActor) {
+		//do nothing
+	}
+	else if (LastActor == nullptr && ThisActor != nullptr) {
+		ThisActor->HighlightActor();
+	}
+	else if (LastActor != nullptr && ThisActor == nullptr) {
+		LastActor->UnHighlightActor();
+	}
+	else {
+		LastActor->UnHighlightActor();
+		ThisActor->HighlightActor();
+	}
+
 }
